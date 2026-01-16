626e91213f2428e9ba1cf2c71a64c080df52e305
Janson <ojt.jpagharion@sminvestments.com>
2026-01-16 15:56:58 +0800

    Moved files for better readability

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "private" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = false  # <-- IMPORTANT
+  map_public_ip_on_launch = false
 }
 
 resource "aws_route_table" "private" {
