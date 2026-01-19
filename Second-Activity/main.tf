c136922594867244150cdd3681be871772d6a26f
Janson <ojt.jpagharion@sminvestments.com>
2026-01-19 16:15:20 +0800

    Removed unnecessary blocks, added comments

--- Second-Activity/main.tf
+++ Second-Activity/main.tf
@@ -1,6 +1,3 @@
-data "http" "my_ip" {
-  url = "https://checkip.amazonaws.com/"
-}
 
 data "aws_availability_zones" "available" {
   state = "available"
@@ -18,6 +15,8 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
+############### SUBNETS
+
 resource "aws_subnet" "public" { #FOR ALB
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.0.0/24"
@@ -28,11 +27,10 @@ resource "aws_subnet" "public" { #FOR ALB
   }
 }
 
-resource "aws_subnet" "co-public" { #FOR ALB
+resource "aws_subnet" "co_public" { #FOR ALB
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = data.aws_availability_zones.available.names[1]
-
 
   tags = {
     Name = "co public subnet"
@@ -49,22 +47,75 @@ resource "aws_subnet" "private" { # FOR EC2
   }
 }
 
+####### ROUTES & ALB LISTENERS
+
 resource "aws_route_table" "test" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = aws_vpc.main.cidr_block
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
   }
 }
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.test.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.co_public.id
+  route_table_id = aws_route_table.test.id
+}
+
+
+# Target group for your app on port 80
+resource "aws_lb_target_group" "app_tg" {
+  name     = "app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    port                = "traffic-port"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    interval            = 30
+    timeout             = 5
+    matcher             = "200-399"
+  }
+}
+
+# HTTP listener (port 80) forwarding to the TG
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.test.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
+# Attach your EC2 instance to the target group
+resource "aws_lb_target_group_attachment" "app_attach" {
+  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_id        = aws_instance.web.id
+  port             = 80
+}
+
+############## ALB & SG
 
 resource "aws_lb" "test" {
   name               = "test-lb-tf"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = aws_security_group.alb_sg.id
+  security_groups    = [aws_security_group.alb_sg.id]
   subnets = [
     aws_subnet.public.id,
-    aws_subnet.co-public.id
+    aws_subnet.co_public.id
   ]
 
   tags = {
@@ -98,7 +149,6 @@ resource "aws_vpc_security_group_ingress_rule" "alb_http_in" {
   to_port           = 80
 }
 
-
 resource "aws_vpc_security_group_egress_rule" "alb_to_app" {
   security_group_id            = aws_security_group.alb_sg.id
   referenced_security_group_id = aws_security_group.ec2_sg.id
@@ -106,6 +156,10 @@ resource "aws_vpc_security_group_egress_rule" "alb_to_app" {
   ip_protocol                  = "tcp"
   to_port                      = 80
 }
+
+
+
+########### EC2 Instance & SG
 
 resource "aws_security_group" "ec2_sg" {
   name        = "ec2_sg"
@@ -133,6 +187,5 @@ resource "aws_instance" "web" {
   ami                         = var.ami_type
   instance_type               = var.instance_type
   subnet_id                   = aws_subnet.private.id
-  vpc_security_group_ids      = aws_security_group.ec2_sg.id
-  associate_public_ip_address = false
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
 }
