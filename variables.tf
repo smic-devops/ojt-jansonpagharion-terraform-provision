6865acd06fe99301e0795c577f7743dc4b166690
Janson <ojt.jpagharion@sminvestments.com>
2026-01-26 16:45:26 +0800

    Created an ALB with an ec2 instance

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description = "AWS region sydney"
+  type        = string
+  default     = "ap-southeast-2"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ami_type" {
+  type        = string
+  description = "Ubuntu AMI ID"
+  default = "ami-0d6621c01e8c2de2c" # Ubuntu Server 20.04 LTS (HVM), SSD Volume Type
+}
